b5977ef11d6f31c0e79a14d68bcf7a1ba5556f81
KevinFalck <kevin.falck@gmail.com>
2025-09-22 11:04:05 +0100

    Update index.html to use correct DOCTYPE and change language to French; remove unnecessary imports in Footer and update alt text in Header component for localization.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Footer() {
   return <div>Copyright Mexican Food 2024</div>;
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 export default function Header() {
   return (
     <header>
       <img
         src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-        alt="Mexican playing guitar"
+        alt="Mexicain jouant de la guitare"
       />
       <nav>
         <ul>
