7b48d057992e7c5348212abf983f8a431b10c78f
KevinFalck <kevin.falck@gmail.com>
2025-09-22 10:55:11 +0100

    Add Footer component to App and enhance Header with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
