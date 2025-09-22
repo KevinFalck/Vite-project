cb0f8e42501dde80b71e5790e264b141ff0e583e
KevinFalck <kevin.falck@gmail.com>
2025-09-22 10:42:57 +0100

    Refactor App structure by removing unused CSS and SVG assets, and simplifying the App component to render only the Header.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <img
+      src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+      alt="logo"
+    />
+  );
+}
