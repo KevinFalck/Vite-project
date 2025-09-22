be2d769ca2c8e36fe380d03ced7be348f9a52eb8
KevinFalck <kevin.falck@gmail.com>
2025-09-22 13:23:20 +0100

    Refactor App component to streamline menu structure by removing unnecessary section wrapper around Dish components.

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,21 @@ function App() {
     <>
       <Header />
       <main>
-        <section>
-          <Dish
-            title="Tacos à l'unité"
-            price="3€"
-            image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-          />
-          <Dish
-            title="Enchiladas"
-            price="12€"
-            image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-          />
-          <Dish
-            title="Mole poblano"
-            price="15€"
-            image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-          />
-        </section>
+        <Dish
+          title="Tacos à l'unité"
+          price="3€"
+          image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+        />
+        <Dish
+          title="Enchiladas"
+          price="12€"
+          image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+        />
+        <Dish
+          title="Mole poblano"
+          price="15€"
+          image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+        />
       </main>
       <Footer />
     </>
