ebefbc195d0f1cfaa212641b01f8a32a6afe564a
KevinFalck <kevin.falck@gmail.com>
2025-09-22 10:59:21 +0100

    Enhance Header component by updating image alt text and restructuring navigation into a list format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,14 @@ export default function Header() {
     <header>
       <img
         src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-        alt="Header Image"
+        alt="Mexican playing guitar"
       />
       <nav>
-        <a href="/">Accueil</a>
+        <ul>
+          <li>
+            <a href="/">Accueil</a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
