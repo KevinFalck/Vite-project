3f57daa0dec5a1935ce836eb603b6934d651cbf8
KevinFalck <kevin.falck@gmail.com>
2025-09-23 10:39:50 +0100

    Add 'isNew' prop to Dish component for new item indication
    Update App component to pass 'isNew' prop to Dish items, allowing for visual differentiation of new dishes with a bootstrap "Nouveau" badge.

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,10 +1,14 @@
 import "../assets/styles/dish.scss";
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Badge } from "react-bootstrap";
 
-const Dish = ({ title, price, image }) => {
+const Dish = ({ title, price, image, isNew }) => {
   return (
     <Card>
+      {isNew && (
+        <Badge className="position-absolute top-0 end-0 m-2">Nouveau</Badge>
+      )}
       <Card.Img variant="top" src={image} alt={title} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
