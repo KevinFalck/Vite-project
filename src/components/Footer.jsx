9efc66a4a4fe81acda4ed86d8ef275d519d158f3
KevinFalck <kevin.falck@gmail.com>
2025-09-23 09:48:25 +0100

    Update Footer styles to enhance visual consistency by adding a grey background to the footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../assets/styles/footer.scss";
 
 export default function Footer() {
   return (
-    <footer className="footer bg-secondary text-white text-center">
+    <footer className="footer text-white text-center">
       <p className="m-0">© Mexican Food 2024</p>
     </footer>
   );
