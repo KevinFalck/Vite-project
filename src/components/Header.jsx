4f7fb19635664cec77979d7a6ab0e6559b965397
KevinFalck <kevin.falck@gmail.com>
2025-09-23 10:06:41 +0100

    Replace header image with a local logo asset for improved loading performance.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Navbar, Nav, Container, Image } from "react-bootstrap";
 import "../assets/styles/header.scss";
+import logo from "../assets/images/logo.webp";
 
 export default function Header() {
   return (
     <Navbar bg="light" expand="md">
       <Container>
         <Navbar.Brand href="/">
-          <Image
-            src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-            alt="Mexicain jouant de la guitare"
-          />
+          <Image src={logo} alt="Mexicain jouant de la guitare" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar" />
         <Navbar.Collapse id="main-navbar">
